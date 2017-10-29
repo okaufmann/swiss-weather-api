fbcf2bf47340359ea0f4dd7ec806b5270156ba95
Oliver Kaufmann <okaufmann91@gmail.com>
2017-10-29 23:40:34 +0100

    Add methods for accessing all parameters

--- src/Client.php
+++ src/Client.php
@@ -328,53 +328,167 @@ class Client
         $this->setupClient();
     }
 
-    public function getPrecipitation($stationId)
-    {
-        $this->getParametersAndVersions();
-        $parameterName = self::PARAMETER_PRECIPITATION;
-        $version = $this->getParameterVersion($parameterName);
-        $station = $this->getStation($version, $parameterName, $stationId);
-
-        return $station;
-    }
-
-    public function getTemperature($stationId)
+    public function getTemperature($stationId, $lang = 'en')
     {
         $this->getParametersAndVersions();
         $parameterName = self::PARAMETER_TEMPERATURE;
         $version = $this->getParameterVersion($parameterName);
-        $station = $this->getStation($version, $parameterName, $stationId);
+        $station = $this->getStation($version, $parameterName, $stationId, $lang);
 
         return $station;
     }
 
-    public function getFoehn($stationId)
+    public function getSunshinePerHour($stationId, $lang = 'en')
     {
         $this->getParametersAndVersions();
-        $parameterName = self::PARAMETER_FOEHN;
+        $parameterName = self::PARAMETER_SUNSHINE;
         $version = $this->getParameterVersion($parameterName);
-        $station = $this->getStation($version, $parameterName, $stationId);
+        $station = $this->getStation($version, $parameterName, $stationId, $lang);
 
         return $station;
     }
 
-    public function getHumidity($stationId)
+    public function getSunshinePerDay($stationId, $lang = 'en')
+    {
+        $this->getParametersAndVersions();
+        $parameterName = self::PARAMETER_SUNSHINE;
+        $version = $this->getParameterVersion($parameterName);
+        $station = $this->getStation($version, $parameterName, $stationId, $lang);
+
+        return $station;
+    }
+
+    public function getPrecipitationPerHour($stationId, $lang = 'en')
+    {
+        $this->getParametersAndVersions();
+        $parameterName = self::PARAMETER_PRECIPITATION;
+        $version = $this->getParameterVersion($parameterName);
+        $station = $this->getStation($version, $parameterName, $stationId, $lang);
+
+        return $station;
+    }
+
+    public function getPrecipitationPerDay($stationId, $lang = 'en')
+    {
+        $this->getParametersAndVersions();
+        $parameterName = self::PARAMETER_PRECIPITATION_YEAR;
+        $version = $this->getParameterVersion($parameterName);
+        $station = $this->getStation($version, $parameterName, $stationId, $lang);
+
+        return $station;
+    }
+
+    public function getWindCombination($stationId, $lang = 'en')
+    {
+        $this->getParametersAndVersions();
+        $parameterName = self::PARAMETER_WIND_COMBINATION;
+        $version = $this->getParameterVersion($parameterName);
+        $station = $this->getStation($version, $parameterName, $stationId, $lang);
+
+        return $station;
+    }
+
+    public function getWindSpeed($stationId, $lang = 'en')
+    {
+        $this->getParametersAndVersions();
+        $parameterName = self::PARAMETER_WIND_SPEED;
+        $version = $this->getParameterVersion($parameterName);
+        $station = $this->getStation($version, $parameterName, $stationId, $lang);
+
+        return $station;
+    }
+
+    public function getWindDirection($stationId, $lang = 'en')
+    {
+        $this->getParametersAndVersions();
+        $parameterName = self::PARAMETER_WIND_DIRECTION;
+        $version = $this->getParameterVersion($parameterName);
+        $station = $this->getStation($version, $parameterName, $stationId, $lang);
+
+        return $station;
+    }
+
+    public function getAirPressure($stationId, $lang = 'en')
+    {
+        $this->getParametersAndVersions();
+        $parameterName = self::PARAMETER_PRESSURE_QFE;
+        $version = $this->getParameterVersion($parameterName);
+        $station = $this->getStation($version, $parameterName, $stationId, $lang);
+
+        return $station;
+    }
+
+    public function getAirPressureSeaLevel($stationId, $lang = 'en')
+    {
+        $this->getParametersAndVersions();
+        $parameterName = self::PARAMETER_PRESSURE_QFF;
+        $version = $this->getParameterVersion($parameterName);
+        $station = $this->getStation($version, $parameterName, $stationId, $lang);
+
+        return $station;
+    }
+
+    public function getAirPressureStandardAtmosphere($stationId, $lang = 'en')
+    {
+        $this->getParametersAndVersions();
+        $parameterName = self::PARAMETER_PRESSURE_QNH;
+        $version = $this->getParameterVersion($parameterName);
+        $station = $this->getStation($version, $parameterName, $stationId, $lang);
+
+        return $station;
+    }
+
+    public function getHumidity($stationId, $lang = 'en')
     {
         $this->getParametersAndVersions();
         $parameterName = self::PARAMETER_HUMIDITY;
         $version = $this->getParameterVersion($parameterName);
-        $station = $this->getStation($version, $parameterName, $stationId);
+        $station = $this->getStation($version, $parameterName, $stationId, $lang);
 
         return $station;
     }
 
-    public function getParameterVersion($parameeterName)
+    public function getSnow($stationId, $lang = 'en')
     {
-        $parameterVersion = $this->parameterVersions->first(function ($item) use ($parameeterName) {
-            return $item['parameter-name'] == $parameeterName;
+        $this->getParametersAndVersions();
+        $parameterName = self::PARAMETER_SNOW_TOTAL;
+        $version = $this->getParameterVersion($parameterName);
+        $station = $this->getStation($version, $parameterName, $stationId, $lang);
+
+        return $station;
+    }
+
+    public function getSnowNew($stationId, $lang = 'en')
+    {
+        $this->getParametersAndVersions();
+        $parameterName = self::PARAMETER_SNOW_NEW;
+        $version = $this->getParameterVersion($parameterName);
+        $station = $this->getStation($version, $parameterName, $stationId, $lang);
+
+        return $station;
+    }
+
+    public function getFoehn($stationId, $lang = 'en')
+    {
+        $this->getParametersAndVersions();
+        $parameterName = self::PARAMETER_FOEHN;
+        $version = $this->getParameterVersion($parameterName);
+        $station = $this->getStation($version, $parameterName, $stationId, $lang);
+
+        return $station;
+    }
+
+    public function getStations($parameterName, $version, $lang = 'en')
+    {
+        $url = 'product/output/measured-values-v2/'.$parameterName.'/'.$version.'/'.$lang.'/overview.json';
+
+        $stations = cache('stations.'.$parameterName, function () use ($lang, $url) {
+            $response = $this->client->get($url);
+            $html = (string)$response->getBody();
+            return json_decode($html, true);
         });
 
-        return $parameterVersion['version'];
+        return $stations['stations'];
     }
 
     public function getParametersAndVersions()
@@ -409,23 +523,17 @@ class Client
         return $this->parameterVersions;
     }
 
-    public function getStations($parameterName, $version)
+    private function getParameterVersion($parameeterName)
     {
-        $lang = 'de';
-        $url = 'product/output/measured-values-v2/'.$parameterName.'/'.$version.'/'.$lang.'/overview.json';
-
-        $stations = cache('stations.'.$parameterName, function () use ($lang, $url) {
-            $response = $this->client->get($url);
-            $html = (string)$response->getBody();
-            return json_decode($html, true);
+        $parameterVersion = $this->parameterVersions->first(function ($item) use ($parameeterName) {
+            return $item['parameter-name'] == $parameeterName;
         });
 
-        return $stations['stations'];
+        return $parameterVersion['version'];
     }
 
-    private function getStation($version, $parameterName, $stationId)
+    private function getStation($version, $parameterName, $stationId, $lang)
     {
-        $lang = 'de';
         $url = 'product/output/measured-values-v2/'.$parameterName.'/'.$version.'/'.$lang.'/'.$stationId.'.json';
 
         $station = cache('station.'.$parameterName, function () use ($lang, $url) {
