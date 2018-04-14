515677ff812d81833d6b9a7c365e13af29a9d99d
Oliver Kaufmann <okaufmann91@gmail.com>
2018-04-14 15:21:30 +0200

    add new metadata client

---
+++ src/Metadata.php
@@ -0,0 +1,89 @@
+<?php
+/*
+ * swiss-weather-api
+ *
+ * This File belongs to to Project swiss-weather-api
+ *
+ * @author Oliver Kaufmann <okaufmann91@gmail.com>
+ * @version 1.0
+ */
+
+namespace Okaufmann\SwissMeteoApi;
+
+use Spatie\Regex\Regex;
+
+
+trait Metadata
+{
+    /**
+     * Searches metadata for a given search term
+     *
+     * @param $queryText
+     * @return array
+     */
+    public function search($queryText)
+    {
+        $normalize = $this->removeAccents($queryText);
+        $normalize = Regex::replace("/[^A-Za-z0-9\s]", '', $normalize);
+        $normalize = strtolower($normalize);
+        $normalize = substr($normalize, 0, 2);
+
+        $url = "/etc/designs/meteoswiss/ajax/search/$normalize.json";
+
+        $result = collect($this->makeRequest($url, true));
+
+        $result = $result->map(function ($r) {
+            list($locationId, $canton, $u1, $postalCode, $u2, $cityName) = explode(';', $r);
+
+            return [
+                'location_id' => $locationId,
+                'canton' => $canton,
+                'postal_code' => $postalCode,
+                'city_name' => $cityName,
+            ];
+        })->filter(function ($m) use ($queryText) {
+            return str_contains($m['cityName'], $queryText) || str_contains($m['postalCode'], $queryText);
+        });
+
+        return $result;
+    }
+
+    /**
+     * Returns metadata for a location.
+     *
+     * @param $locationId
+     * @return array
+     */
+    public function getMetadata($locationId)
+    {
+        if (strlen($locationId) !== 6 || !Regex::match("/[0-9]{6}/", $locationId)->hasMatch()) {
+            throw new \InvalidArgumentException('locationId must be a id out of 6 numbers');
+        }
+
+        $url = "/etc/designs/meteoswiss/ajax/location/$locationId.json";
+
+        $result = collect($this->makeRequest($url, true));
+
+        return [
+            'location_id' => $result['location_id'],
+            'city_name' => $result['city_name'],
+            'postal_code' => substr($result['location_id'], 0, 4),
+            'station_id' => $result['station_id'],
+            'webcam_id' => $result['webcam_id'],
+        ];
+    }
+
+    /**
+     * Replace accented characters with non accented
+     *
+     * @param $str
+     * @return mixed
+     * @link http://myshadowself.com/coding/php-function-to-convert-accented-characters-to-their-non-accented-equivalant/
+     */
+    private function removeAccents($str)
+    {
+        $a = array('À', 'Á', 'Â', 'Ã', 'Ä', 'Å', 'Æ', 'Ç', 'È', 'É', 'Ê', 'Ë', 'Ì', 'Í', 'Î', 'Ï', 'Ð', 'Ñ', 'Ò', 'Ó', 'Ô', 'Õ', 'Ö', 'Ø', 'Ù', 'Ú', 'Û', 'Ü', 'Ý', 'ß', 'à', 'á', 'â', 'ã', 'ä', 'å', 'æ', 'ç', 'è', 'é', 'ê', 'ë', 'ì', 'í', 'î', 'ï', 'ñ', 'ò', 'ó', 'ô', 'õ', 'ö', 'ø', 'ù', 'ú', 'û', 'ü', 'ý', 'ÿ', 'Ā', 'ā', 'Ă', 'ă', 'Ą', 'ą', 'Ć', 'ć', 'Ĉ', 'ĉ', 'Ċ', 'ċ', 'Č', 'č', 'Ď', 'ď', 'Đ', 'đ', 'Ē', 'ē', 'Ĕ', 'ĕ', 'Ė', 'ė', 'Ę', 'ę', 'Ě', 'ě', 'Ĝ', 'ĝ', 'Ğ', 'ğ', 'Ġ', 'ġ', 'Ģ', 'ģ', 'Ĥ', 'ĥ', 'Ħ', 'ħ', 'Ĩ', 'ĩ', 'Ī', 'ī', 'Ĭ', 'ĭ', 'Į', 'į', 'İ', 'ı', 'Ĳ', 'ĳ', 'Ĵ', 'ĵ', 'Ķ', 'ķ', 'Ĺ', 'ĺ', 'Ļ', 'ļ', 'Ľ', 'ľ', 'Ŀ', 'ŀ', 'Ł', 'ł', 'Ń', 'ń', 'Ņ', 'ņ', 'Ň', 'ň', 'ŉ', 'Ō', 'ō', 'Ŏ', 'ŏ', 'Ő', 'ő', 'Œ', 'œ', 'Ŕ', 'ŕ', 'Ŗ', 'ŗ', 'Ř', 'ř', 'Ś', 'ś', 'Ŝ', 'ŝ', 'Ş', 'ş', 'Š', 'š', 'Ţ', 'ţ', 'Ť', 'ť', 'Ŧ', 'ŧ', 'Ũ', 'ũ', 'Ū', 'ū', 'Ŭ', 'ŭ', 'Ů', 'ů', 'Ű', 'ű', 'Ų', 'ų', 'Ŵ', 'ŵ', 'Ŷ', 'ŷ', 'Ÿ', 'Ź', 'ź', 'Ż', 'ż', 'Ž', 'ž', 'ſ', 'ƒ', 'Ơ', 'ơ', 'Ư', 'ư', 'Ǎ', 'ǎ', 'Ǐ', 'ǐ', 'Ǒ', 'ǒ', 'Ǔ', 'ǔ', 'Ǖ', 'ǖ', 'Ǘ', 'ǘ', 'Ǚ', 'ǚ', 'Ǜ', 'ǜ', 'Ǻ', 'ǻ', 'Ǽ', 'ǽ', 'Ǿ', 'ǿ', 'Ά', 'ά', 'Έ', 'έ', 'Ό', 'ό', 'Ώ', 'ώ', 'Ί', 'ί', 'ϊ', 'ΐ', 'Ύ', 'ύ', 'ϋ', 'ΰ', 'Ή', 'ή');
+        $b = array('A', 'A', 'A', 'A', 'A', 'A', 'AE', 'C', 'E', 'E', 'E', 'E', 'I', 'I', 'I', 'I', 'D', 'N', 'O', 'O', 'O', 'O', 'O', 'O', 'U', 'U', 'U', 'U', 'Y', 's', 'a', 'a', 'a', 'a', 'a', 'a', 'ae', 'c', 'e', 'e', 'e', 'e', 'i', 'i', 'i', 'i', 'n', 'o', 'o', 'o', 'o', 'o', 'o', 'u', 'u', 'u', 'u', 'y', 'y', 'A', 'a', 'A', 'a', 'A', 'a', 'C', 'c', 'C', 'c', 'C', 'c', 'C', 'c', 'D', 'd', 'D', 'd', 'E', 'e', 'E', 'e', 'E', 'e', 'E', 'e', 'E', 'e', 'G', 'g', 'G', 'g', 'G', 'g', 'G', 'g', 'H', 'h', 'H', 'h', 'I', 'i', 'I', 'i', 'I', 'i', 'I', 'i', 'I', 'i', 'IJ', 'ij', 'J', 'j', 'K', 'k', 'L', 'l', 'L', 'l', 'L', 'l', 'L', 'l', 'l', 'l', 'N', 'n', 'N', 'n', 'N', 'n', 'n', 'O', 'o', 'O', 'o', 'O', 'o', 'OE', 'oe', 'R', 'r', 'R', 'r', 'R', 'r', 'S', 's', 'S', 's', 'S', 's', 'S', 's', 'T', 't', 'T', 't', 'T', 't', 'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u', 'W', 'w', 'Y', 'y', 'Y', 'Z', 'z', 'Z', 'z', 'Z', 'z', 's', 'f', 'O', 'o', 'U', 'u', 'A', 'a', 'I', 'i', 'O', 'o', 'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u', 'A', 'a', 'AE', 'ae', 'O', 'o', 'Α', 'α', 'Ε', 'ε', 'Ο', 'ο', 'Ω', 'ω', 'Ι', 'ι', 'ι', 'ι', 'Υ', 'υ', 'υ', 'υ', 'Η', 'η');
+        return str_replace($a, $b, $str);
+    }
+}
